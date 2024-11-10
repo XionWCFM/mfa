3f4ddcfb86afdb526eabe1458a17db9e4a8cc879
devgiljong <calf-love@naver.com>
2024-11-10 18:50:39 +0900

    :construction: Enum code add

--- apps/mobile/src/bridge/native-to-webview-event.ts
+++ apps/mobile/src/bridge/native-to-webview-event.ts
@@ -1,23 +1,8 @@
-import { validate } from "../utils/validate";
 import { z } from "zod";
+import { createEnum } from "../utils/create-enum";
+import { validate } from "../utils/validate";
 
-const NATIVE_TO_WEBVIEW_EVENT_LIST = [
-  "UPLOAD_IMAGE_RESULT",
-  "DOWNLOAD_IMAGE_RESULT",
-] as const;
-
-export type NativeToWebviewEvent =
-  (typeof NATIVE_TO_WEBVIEW_EVENT_LIST)[number];
-
-export const NATIVE_TO_WEBVIEW_EVENT = NATIVE_TO_WEBVIEW_EVENT_LIST.reduce(
-  (acc, cur) => {
-    //@ts-expect-error
-    acc[cur] = cur;
-    return acc;
-  },
-  {} as { [K in NativeToWebviewEvent]: K }
-);
-
-export const validateNativeToWebviewEvent = validate<NativeToWebviewEvent>(
-  z.enum(NATIVE_TO_WEBVIEW_EVENT_LIST)
-);
+const NATIVE_TO_WEBVIEW_EVENT_LIST = ["UPLOAD_IMAGE_RESULT", "DOWNLOAD_IMAGE_RESULT"] as const;
+export type NativeToWebviewEvent = (typeof NATIVE_TO_WEBVIEW_EVENT_LIST)[number];
+export const NATIVE_TO_WEBVIEW_EVENT = createEnum(NATIVE_TO_WEBVIEW_EVENT_LIST);
+export const validateNativeToWebviewEvent = validate<NativeToWebviewEvent>(z.enum(NATIVE_TO_WEBVIEW_EVENT_LIST));
--- apps/mobile/src/bridge/webview-to-native-event.ts
+++ apps/mobile/src/bridge/webview-to-native-event.ts
@@ -1,5 +1,6 @@
-import { validate } from "../utils/validate";
 import { z } from "zod";
+import { createEnum } from "../utils/create-enum";
+import { validate } from "../utils/validate";
 
 const WEBVIEW_TO_NATIVE_EVENT_LIST = [
   "OPEN_WEB_BROWSER",
@@ -8,20 +9,7 @@ const WEBVIEW_TO_NATIVE_EVENT_LIST = [
   "DOWNLOAD_IMAGE",
 ] as const;
 
-export type WebviewToNativeEvent =
-  (typeof WEBVIEW_TO_NATIVE_EVENT_LIST)[number];
+export type WebviewToNativeEvent = (typeof WEBVIEW_TO_NATIVE_EVENT_LIST)[number];
+export const WEBVIEW_TO_NATIVE_EVENT = createEnum(WEBVIEW_TO_NATIVE_EVENT_LIST);
+export const validateWebviewToNativeEvent = validate<WebviewToNativeEvent>(z.enum(WEBVIEW_TO_NATIVE_EVENT_LIST));
 
-export const WEBVIEW_TO_NATIVE_EVENT = WEBVIEW_TO_NATIVE_EVENT_LIST.reduce(
-  (acc, cur) => {
-    //@ts-expect-error
-    acc[cur] = cur;
-    return acc;
-  },
-  {} as { [K in WebviewToNativeEvent]: K }
-);
-
-export const validateWebviewToNativeEvent = validate<WebviewToNativeEvent>(
-  z.enum(WEBVIEW_TO_NATIVE_EVENT_LIST)
-);
-
-const r = WEBVIEW_TO_NATIVE_EVENT.OPEN_WEB_BROWSER;
