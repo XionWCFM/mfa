21f432742eaff42e06b1e39c3baf72c730ac55e6
myjeong19 <myjeong19@naver.com>
2024-11-12 04:05:26 +0900

    ✨ Webview-bridge
    Connecting React and React Native a webview-bridge

--- apps/host/src/routes/page.tsx
+++ apps/host/src/routes/page.tsx
@@ -3,6 +3,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { Button } from "@xionwcfm/xds/button";
 import { ConfirmDialog } from "@xionwcfm/xds/confirm-dialog";
 import { overlay } from "overlay-kit";
+import Bridge from "remote/bridge";
 import Hi from "remote/button";
 import { useCount } from "remote/store";
 
@@ -14,6 +15,8 @@ const Index = () => {
     <div className="container-box">
       <Hi />
       <Button onClick={increment}>{count}</Button>
+      <hr />
+      <Bridge />
     </div>
   );
 };
