1c84c5f3f37cb0a6bb71106d933002aca729372a
devgiljong <calf-love@naver.com>
2024-11-28 12:08:51 +0900

    feat: bump deps

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,21 +1,58 @@
-import ky from "ky";
+import ky, { KyInstance, Options, HTTPError } from "ky";
 
-const http = {
-  get: function get<Response = unknown>(url: string): Promise<Response> {
-    return ky.get(`${url}`).then((response) => response.json());
-  },
-  post: function post<Request = any, Response = unknown>(url: string, payload?: Request): Promise<Response> {
-    return ky.post(`${url}`, { json: payload }).then((response) => response.json());
-  },
-  put: function post<Request = any, Response = unknown>(url: string, payload?: Request): Promise<Response> {
-    return ky.put(`${url}`, { json: payload }).then((response) => response.json());
-  },
-  delete: function kydelete<Response = unknown>(url: string): Promise<Response> {
-    return ky.delete(`${url}`).then((response) => response.json());
-  },
-  patch: function patch<Request = any, Response = unknown>(url: string, payload?: Request): Promise<Response> {
-    return ky.patch(`${url}`, { json: payload }).then((response) => response.json());
-  },
+function isKyInstance(instance: any): instance is KyInstance {
+  return instance && typeof instance.get === "function" && typeof instance.post === "function";
+}
+
+export const isHttpError = (error: unknown): error is HTTPError => {
+  return error instanceof HTTPError;
 };
 
-export default http;
+export const createInstance = (options?: Options) => ky.create(options);
+
+export const createHttp = (instance?: KyInstance | Options) => {
+  const kyInstance = isKyInstance(instance) ? instance : createInstance(instance);
+
+  const http = {
+    get: async function get<Response = unknown>(url: string, options?: Options): Promise<Response> {
+      const response = await kyInstance.get(`${url}`, options);
+      return response.json();
+    },
+    post: async function post<Request = any, Response = unknown>(
+      url: string,
+      payload?: Request,
+      options?: Options,
+    ): Promise<Response> {
+      const response = await kyInstance.post(`${url}`, { json: payload, ...options });
+      return response.json();
+    },
+    put: async function put<Request = any, Response = unknown>(
+      url: string,
+      payload?: Request,
+      options?: Options,
+    ): Promise<Response> {
+      const response = await kyInstance.put(`${url}`, { json: payload, ...options });
+
+      return response.json();
+    },
+    delete: async function kydelete<Response = unknown>(url: string, options: Options): Promise<Response> {
+      const response = await kyInstance.delete(`${url}`, options);
+
+      return response.json();
+    },
+    patch: async function patch<Request = any, Response = unknown>(
+      url: string,
+      payload?: Request,
+      options?: Options,
+    ): Promise<Response> {
+      const response = await kyInstance.patch(`${url}`, { json: payload, ...options });
+
+      return response.json();
+    },
+  };
+  return http;
+};
+
+export type HttpInstance = ReturnType<typeof createHttp>;
+
+export const http = createHttp();
